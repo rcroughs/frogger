25906c6441af6b0ec87aa12023a99c4c093244de
itsKorzie <romain.croughs@gmail.com>
2023-11-17 15:03:45 +0100

    fix: included logic of getters and setters in Game

--- src/Game.h
+++ src/Game.h
@@ -8,10 +8,11 @@ private:
 public:
     Game(Player player, Map map) : player{player}, map{map} {}
     virtual ~Game();
-    virtual Player getPlayer();
-    virtual void setPlayer(Player new_player);
-    virtual Map getMap();
-    virtual void setMap();
+
+    virtual Player getPlayer() {return this->player;}
+    virtual void setPlayer(Player new_player) {player = new_player;}
+    virtual Map getMap() {return this->map;}
+    virtual void setMap(Map new_map) {map = new_map;}
 };
 
 
