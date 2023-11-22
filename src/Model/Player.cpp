2ef5782b914456de126bfc5c72a4db8e67550a3c
itsKorzie <romain.croughs@gmail.com>
2023-11-22 15:30:36 +0100

    feat: delitimitage des inputs

--- src/Model/Player.cpp
+++ src/Model/Player.cpp
@@ -1,17 +1,25 @@
 #include "Player.h"
 
 void Player::move_up() {
-    current_position.y = current_position.y + 1;
+    if (current_position.y < 12) {
+        current_position.y = current_position.y + 1;
+    }
 }
 
 void Player::move_down() {
-    current_position.y = current_position.y - 1;
+    if (current_position.y > 0) {
+        current_position.y = current_position.y - 1;
+    }
 }
 
 void Player::move_right() {
-    current_position.x = current_position.x + 1;
+    if (current_position.x < 100) {
+        current_position.x = current_position.x + 1;
+    }
 }
 
 void Player::move_left() {
-    current_position.x = current_position.x - 1;
+    if (current_position.x > 0) {
+        current_position.x = current_position.x - 1;
+    }
 }
