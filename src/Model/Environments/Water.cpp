11355b61bf2da9b8d74a0bb154edfc789d604ec9
lucas <lucasvanpraag@hotmail.fr>
2023-11-30 03:18:53 +0100

    Fix : génération de la taille

--- src/Model/Environments/Water.cpp
+++ src/Model/Environments/Water.cpp
@@ -5,12 +5,8 @@ Water::Water() : isMoving{true}, color(FL_BLUE) {
     int logCounter = 0;
     while (usedSpace + 20 < 100) {
         srand(clock());
-        // Génération de la taille de la bûche (30, 40, ou 70 % de l'écran en longueur)
-        int size = 0;
-        while (size != 1 && size != 2 && size != 3) {
-            size = rand() % 7;
-        }
-        size *= 10;
+        // Génération de la taille de la bûche
+        int size = (rand() % 3 + 1) * 10;;
         // Génération de la position
         float position = 0;
         if (logCounter - 1 >= 0) {
