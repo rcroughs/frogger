5aeafb71e51d2f0694bb690489f1cb6d019ea389
Romain Croughs <romain.croughs@gmail.com>
2023-12-19 14:12:16 +0100

    fix: implement pure virtual function

--- src/Controller/EditorController.h
+++ src/Controller/EditorController.h
@@ -20,6 +20,7 @@ public:
     virtual void keyPressed(int keycode) override {};
     virtual void keyReleased(int keycode) override {};
     virtual void updateMovement() override {};
+    virtual void mouseRelease(short loc_x, short loc_y) {}
 };
 
 #endif //SRC_EDITORCONTROLLER_H
