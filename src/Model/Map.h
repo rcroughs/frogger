439b8c58fd1b986ebf7461763d4c5c14659223e9
Romain Croughs <romain.croughs@gmail.com>
2024-01-11 13:36:52 +0100

    docs: maps documentation

--- src/Model/Map.h
+++ src/Model/Map.h
@@ -15,23 +15,51 @@ enum Speed { SLOW = 1, MEDIUM = 2, FAST = 3 };
 
 class Map {
 public:
+  /*
+   * Without any arguments (deprecated)
+   */
   Map();
+  /*
+   * Creates a map with the given mapId.
+   */
   Map(std::string mapId);
+
+  /*
+   * Getters and setters for the environments array.
+   * Does not take any arguments.
+   */
   [[nodiscard]] virtual std::shared_ptr<Environment> getEnvironment(int index) const;
   virtual void deleteEnvironment(int index);
   virtual void setEnvironment(int index,
                               std::shared_ptr<Environment> new_environment);
   virtual void resetEnvironments();
 
+  /*
+   * Updates the props of all environments.
+   * Does not take any arguments and returns void
+   */
   virtual void updateProps() const;
+  /*
+  * Handles the game
+  * Takes the game running
+  */
   virtual void handleGame(Game *currentGame) const;
 
+  /*
+   * Creates all the environments with the given mapId.
+   * Takes the mapId as a string and returns a boolean.
+   */
   virtual bool createMap(std::string mapId);
 
+  /*
+   * Returns the mapId of the map.
+   * Does not take any arguments and returns a string.
+   */
   virtual std::string getMapId() const;
 
 protected:
 
+  // Subfunctions for createMap.
   virtual bool createLogEnvironment(std::string mapId, int &stringCount, int environmentCount);
   virtual bool createTurtleEnvironment(std::string mapId, int &stringCount, int environmentCount);
   virtual bool createRoadEnvironment(std::string mapId, int &stringCount, int environmentCount);
